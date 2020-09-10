440c974da02fe1202e6bed540c2928cf1edb7735
Rodrigo Brito <rig.10@hotmail.com>
2020-09-10 19:54:41 -0300

    inclui filtro de tamanho e vogais

--- main.py
+++ main.py
@@ -2,8 +2,24 @@
 
 import sys
 
-if len(sys.argv) != 3:
-    print("example of usage: ./main.py old.txt new.txt")
+def count_vowels(sentence):
+    count = 0
+    for letter in sentence:
+        if letter in ['a', 'e', 'i', 'o', 'u']:
+            count += 1
+    return count
+
+if len(sys.argv) < 3:
+    print("example of usage: ./main.py old.txt new.txt [max_size] [vowels]")
+
+size = 10
+vowels = 2
+
+if len(sys.argv) > 3:
+    size = int(sys.argv[3])
+
+if len(sys.argv) > 4:
+    vowels = int(sys.argv[4])
 
 oldDomains = []
 with open(sys.argv[1]) as domains:
@@ -18,5 +34,7 @@ with open(sys.argv[2]) as domains:
 includes = set(newDomains).difference(oldDomains)
 
 for include in includes:
-	if ".com.br" in include:
-		print(include)
+    if ".com.br" in include:
+        prefix = include.replace(".com.br", "")
+        if len(prefix) <= size and count_vowels(prefix) >= vowels:
+            print(include)
